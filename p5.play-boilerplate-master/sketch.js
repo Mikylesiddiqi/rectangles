6810342fa1c15131134ec531ba8ec6339a53aad3
Mikylesiddiqi <71292256+Mikylesiddiqi@users.noreply.github.com>
2020-09-23 17:02:35 -0400

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,32 @@
+var fixed_rect, moving_rect; 
+
+function setup() {
+  createCanvas(1200,800);
+  fixed_rect=createSprite(200, 200, 50, 80);
+  moving_rect=createSprite(400, 200, 80, 30);
+  moving_rect.shapeColor="red";
+  fixed_rect.shapeColor="red";
+  fixed_rect.debug=true;
+  moving_rect.debug=true;
+}
+
+function draw() {
+  background(255,255,255);  
+  moving_rect.x=World.mouseX;
+  moving_rect.y=World.mouseY;
+  if(moving_rect.x-fixed_rect.x<fixed_rect.width/2+moving_rect.width/2 &&
+     fixed_rect.x-moving_rect.x<fixed_rect.width/2+moving_rect.width/2 &&
+      moving_rect.y-fixed_rect.y<moving_rect.height/2+fixed_rect.height/2 &&
+      fixed_rect.y-moving_rect.y<fixed_rect.height/2+moving_rect.height/2){
+    moving_rect.shapeColor="green";
+    fixed_rect.shapeColor="green";
+  }
+  else{
+    moving_rect.shapeColor="red";
+    fixed_rect.shapeColor="red";
+
+  }
+  
+  drawSprites();
+
+}
